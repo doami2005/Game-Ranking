6f3c99e03222cf8cc5646e89e3e760ad1ff28225
doami2005 <54644531+doami2005@users.noreply.github.com>
2020-07-29 14:27:37 +0900

    Update getGameRank.js

--- getGameRank.js
+++ getGameRank.js
@@ -2,7 +2,7 @@ const Jsoup = org.jsoup.Jsoup;
 const Lw = '\u200b'.repeat(1000);
 const Ll = '⎼'.repeat(50);
 
-function getGameRanking() {
+function getGameRanking(n) {
     let parse = Jsoup.connect('http://gevolution.co.kr/rank/aos').get().select('.rank1').toArray()
                 .filter((_, i) => i%3 == 0)
                 .map((e, i) => ++i+'위 | '+e.text()).join('\n');
@@ -10,5 +10,5 @@ function getGameRanking() {
     '• 모바일 무료 게임 순위입니다.'+Lw,
     Ll, parse, Ll, 'doami'
     ].join('\n');
-    return result;
+    return n ? [result] : result;
 };
